6594a55adc8e1e4968a4156cf0767196ea1bc6a0
ricardo-xml <61173388+ricardo-xml@users.noreply.github.com>
2020-10-03 10:52:25 -0700

    Add files via upload

---
+++ Animaciones/Assets/Script/CharacterMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CharacterMove : MonoBehaviour
+{
+    public float vMove = 5.0f;
+    public float vRotation = 200.0f;
+    private Animator animator;
+    public float x, y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        transform.Rotate(0, x*Time.deltaTime*vRotation,0);
+        transform.Translate(0, 0, y * Time.deltaTime * vMove);
+        animator.SetFloat("VelX", x);
+        animator.SetFloat("VelY", y);
+    }
+}
