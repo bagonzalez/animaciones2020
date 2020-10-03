5f45dca4135e04adfa4ed0b20d7a06784ede4895
Rodrigo Baires <rodrigobaires13@gmail.com>
2020-10-02 20:52:35 -0600

    animacion esqueleto

---
+++ Assets/Animaciones.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animaciones : MonoBehaviour{
+    Animator animator;
+
+    void Awake () {
+        animator = GetComponent<Animator> ();
+    }
+
+    void Update () {
+        bool caminar = Input.GetKey (KeyCode.LeftControl);
+            animator.SetBool ("Caminar", caminar);
+
+        if (Input.GetKey (KeyCode.Space)) {
+            animator.SetTrigger ("Atacar");
+        }
+    }
+}
